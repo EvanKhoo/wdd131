cf615684ddcc3b94242f6eb5928cab5d72e37ad7
EvanKhoo <kho21002@byui.edu>
2025-09-19 17:53:20 -0600

    Prove 1 BYUI mission statement

---
+++ ponder/meta-css.js
@@ -0,0 +1,23 @@
+const PI = 3.14;
+const ONE = 1;
+const TWO = '2';
+let radius = 3;
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+
+area = radius * radius * PI;
+
+console.log(area);
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
