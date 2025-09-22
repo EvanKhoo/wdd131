9e39ce6b6ac4767f14fc55ee8754fed08a40fc88
EvanKhoo <kho21002@byui.edu>
2025-09-22 12:30:34 -0600

    dom ponder

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -24,12 +24,21 @@ console.log(result);
 result = ONE + Number(TWO);
 console.log(result);
 
-let course = "CSE131"; //global scope
-if (true) {
-    let student = "John";
-    console.log(course);  //works just fine, course is global
-    console.log(student); //works just fine, it's being accessed within the block
-}
-console.log(course); //works fine, course is global
-console.log(student); //does not work, can't access a block variable outside the block
+//let course = "CSE131"; //global scope
+//if (true) {
+    //let student = "John";
+   // console.log(course);  //works just fine, course is global
+ //   console.log(student); //works just fine, it's being accessed within the block
+//}
+//console.log(course); //works fine, course is global
+//console.log(student); //does not work, can't access a block variable outside the block
                     
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+})
+                
